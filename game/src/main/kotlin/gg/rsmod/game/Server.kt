9618d0e7211a4cee94db389d09610021e088523b
= <admin@indovaps.com>
2019-07-16 10:52:31 -0400

    Initial commit

---
+++ game/src/main/kotlin/gg/rsmod/game/Server.kt
@@ -0,0 +1,13 @@
+package gg.rsmod.game
+
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * @author Tom <rspsmods@gmail.com>
+ */
+class Server {
+
+    val ioDispatcher = Dispatchers.IO
+
+    val blockDispatcher = Dispatchers.Default
+}
