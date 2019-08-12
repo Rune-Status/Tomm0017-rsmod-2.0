3b7ea4f94b9159e0db3d81ae44933b279d9ddc6a
Tom0017 <rspsmods@gmail.com>
2019-08-12 00:03:42 -0400

    Add initial tests

---
+++ game/src/test/kotlin/gg/rsmod/game/ServerTests.kt
@@ -0,0 +1,19 @@
+package gg.rsmod.game
+
+import com.google.inject.Guice
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ServerTests {
+
+    @Test
+    fun `construct with dependency injection`() {
+        val module = ServerModule()
+        val guice = Guice.createInjector(module)
+
+        val server = guice.getInstance(Server::class.java)
+        assertNotNull(server)
+    }
+}
